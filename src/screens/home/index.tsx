1af8cf3b14cbcc8a2f9ccbdb72d013c1ea39776f
marlonerick <erick.marlon13@hotmail.com>
2023-02-13 16:50:09 -0300

    Add comma operation

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -27,11 +27,12 @@ export function Home() {
   const [oldNum, setOldNum] = useState(0)
   const [operator, setOperator] = useState('')
 
-  // const [result, setResult] = useState('0')
+  const [conta, setConta] = useState()
+
 
   //Pega numero
   function handleClickNumber(value: any) {
-    if (num === 0) {
+    if (num == 0) {
       setNum(value)
     } else {
       setNum(num + value)
@@ -69,17 +70,32 @@ export function Home() {
 
   function calculate() {
 
-    if (operator === "/") {
-      setNum(oldNum / num)
-    } else if (operator === "X") {
-      setNum(oldNum * num)
-    } else if (operator === "-") {
-      setNum(oldNum - num)
-    } else if (operator === "+") {
-      setNum(Number(oldNum) + Number(num))
-    }
+    if (operator == "/") {
 
-    console.log(num, oldNum, operator)
+      setNum(oldNum / num)
+
+    } else if (operator == "X") {
+
+      var number1 = parseFloat(oldNum.toString().replace(',', '.'))
+      var number2 = parseFloat(num.toString().replace(',', '.'))
+
+      setNum(number1 * number2)
+
+    } else if (operator == "-") {
+
+      var number1 = parseFloat(oldNum.toString().replace(',', '.'))
+      var number2 = parseFloat(num.toString().replace(',', '.'))
+
+      setNum(number1 - number2)
+
+    } else if (operator == "+") {
+
+      var number1 = parseFloat(oldNum.toString().replace(',', '.'))
+      var number2 = parseFloat(num.toString().replace(',', '.'))
+
+      setNum(Number(number1) + Number(number2))
+
+    }
   }
 
   return (
@@ -89,68 +105,61 @@ export function Home() {
         <div className="w-full h-28 bg-zinc-500 rounded-xl px-2 py-2">
 
           {
-            num !== 0 && <h5 className="flex justify-end content-end text-white font-semibold text-2xl opacity-50">old</h5>
+            num !== 0 && <h5 className="flex justify-end content-end text-white font-semibold text-2xl opacity-50">{conta}</h5>
           }
           <h6 className="flex justify-end content-end py-1 text-white font-semibold text-6xl">{num}</h6>
         </div>
 
-        <div className="grid grid-cols-4 gap-6 my-5">
+        <div className="grid grid-cols-4 gap-5 my-5">
           {
             numbers.map((numbers, index) => {
-              if (numbers === '+' || numbers === '-' || numbers === 'X' || numbers === '/') {
+              if (numbers == '+' || numbers == '-' || numbers == 'X' || numbers == '/') {
                 return (
                   <div key={index}>
-                    <button className="w-14 h-14 rounded-3xl bg-violet-900 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers} onClick={operatorHandler}>{numbers}</button>
+                    <button className="w-16 h-16 rounded-3xl bg-violet-800 text-white text-xl font-bold shadow-lg shadow-violet-500/20 transition hover:bg-violet-500 duration-300" value={numbers} onClick={operatorHandler}>{numbers}</button>
                   </div>
                 )
 
               } else {
-                if (numbers === 'C' || numbers === '+/-' || numbers === '%') {
+                if (numbers == 'C' || numbers == '+/-' || numbers == '%') {
 
-                  if (numbers === 'C') {
+                  if (numbers == 'C') {
                     return (
                       <div key={index}>
-                        <button className="w-14 h-14 rounded-3xl bg-zinc-500 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers} onClick={clean}>{numbers}</button>
+                        <button className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-zinc-300 duration-300" value={numbers} onClick={clean}>{numbers}</button>
                       </div>
                     )
                   }
-                  else if (numbers === '%') {
+                  else if (numbers == '%') {
                     return (
                       <div key={index}>
-                        <button className="w-14 h-14 rounded-3xl bg-zinc-500 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers} onClick={porcetage}>{numbers}</button>
+                        <button className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-zinc-300 duration-300" value={numbers} onClick={porcetage}>{numbers}</button>
                       </div>
                     )
                   }
-                  else if (numbers === '+/-') {
+                  else if (numbers == '+/-') {
                     return (
                       <div key={index}>
-                        <button className="w-14 h-14 rounded-3xl bg-zinc-500 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers} onClick={changeSignal}>{numbers}</button>
+                        <button className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-zinc-300 duration-300" value={numbers} onClick={changeSignal}>{numbers}</button>
                       </div>
                     )
                   }
                   return (
                     <div key={index}>
-                      <button className="w-14 h-14 rounded-3xl bg-zinc-500 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers}>{numbers}</button>
+                      <button className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-zinc-300 duration-300" value={numbers}>{numbers}</button>
                     </div>
                   )
                 }
-                else if (numbers === '=') {
+                else if (numbers == '=') {
                   return (
                     <div key={index}>
-                      <button className="w-36 h-14 rounded-3xl bg-zinc-500 text-white font-semibold shadow-lg shadow-violet-500/20" value={numbers} onClick={calculate}>{numbers}</button>
-                    </div>
-                  )
-                }
-                else if (numbers === ',') {
-                  return (
-                    <div key={index} >
-                      <button name={numbers} value={numbers} onClick={() => handleClickNumber(numbers)} className="w-14 h-14 rounded-3xl bg-zinc-800 text-white font-semibold shadow-lg shadow-violet-500/20">{numbers}</button>
+                      <button className="w-36 h-16 rounded-3xl bg-violet-800 text-white text-3xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-violet-500 duration-500 " value={numbers} onClick={calculate}>{numbers}</button>
                     </div>
                   )
                 }
                 return (
                   <div key={index} >
-                    <button name={numbers} value={numbers} onClick={() => handleClickNumber(numbers)} className="w-14 h-14 rounded-3xl bg-zinc-800 text-white font-semibold shadow-lg shadow-violet-500/20">{numbers}</button>
+                    <button name={numbers} value={numbers} onClick={() => handleClickNumber(numbers)} className="w-16 h-16 rounded-3xl bg-zinc-800 text-white text-2xl font-semibold shadow-lg shadow-violet-500/20 transition hover:bg-zinc-600 duration-500 ">{numbers}</button>
                   </div>
                 )
               }

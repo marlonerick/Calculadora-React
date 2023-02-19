3e727c2025ced4a3410e34033852f4948e686448
marlonerick <erick.marlon13@hotmail.com>
2023-02-19 16:25:39 -0300

    Fixed Bug

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-
 type Valor = {
 
     valor: number;
 }
 export function Display(props: Valor) {
-    const [test, setTest] = useState()
+
     return (
         <>
             <input type={"text"} maxLength={2} value={props.valor} className="w-full h-28 py-1 my-1 rounded-xl bg-zinc-500 text-white text-end font-semibold text-6xl transition focus:outline focus:ring-2 focus:ring-violet-500  focus:ring-offset-2 focus:ring-offset-background"></input>

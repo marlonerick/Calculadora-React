b094d8a3900f4f0ab876d939e80bac85fe56364f
marlonerick <erick.marlon13@hotmail.com>
2023-03-18 10:10:05 -0300

    Design Reponsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ export function App() {
 
 
   return (
-    <div className='w-screen h-screen bg-white dark:bg-gradient-to-r from-cyan-300 to-blue-600 flex items-center justify-center'>
+    <div className='
+    w-screen h-screen bg-white bg-gradient-to-r from-cyan-300 to-blue-600 
+    dark:bg-gradient-to-tr dark:from-slate-600 dark:to-zinc-900 
+    flex items-center justify-center
+
+    // responsive
+    sm:w-screen sm:h-screen sm:flex sm:items-center sm:justify-center
+    md:w-screen md:h-screen md:flex md:items-center md:justify-center
+    lg:w-screen lg:h-screen lg:flex lg:items-center lg:justify-center
+    xl:w-screen xl:h-screen xl:flex xl:items-center xl:justify-center
+    '>
       <Home />
     </div>
   )
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -105,17 +105,31 @@ export function Home() {
       <div>
         <Theme />
       </div>
-      <div className="w-96 h-2/2 px-5 py-5 bg-zinc-400 dark:bg-zinc-900 rounded-3xl border-4 border-zinc-600">
+      <div className="
+      w-96 h-1/2 px-5 py-5 bg-zinc-400 dark:bg-zinc-900 rounded-3xl border-4 border-zinc-600
+      sm:w-2/4 sm:h-1/2 sm:px-4 sm:py-4
+      md:w-5/12 md:h-1/2 md:px-2 md:py-2
+      xl:w-5/12 xl:h-1/3 xl:px-4 xl:py-4
+      ">
 
         <Display valor={num}></Display>
 
-        <div className="grid grid-cols-4 gap-6 my-5">
+        <div className="grid grid-cols-4 gap-6 my-5
+        sm:grid sm:grid-cols-4 sm:gap-3 sm:m-2
+        md:grid md:grid-cols-4 md:gap-4 md:m-2
+        xl:grid xl:grid-cols-4 xl:gap-5 xl:m-4
+        ">
           {
             numbers.map((numbers, index) => {
               if (numbers == '+' || numbers == '-' || numbers == 'X' || numbers == '/') {
                 return (
                   <div key={index}>
-                    <button value={numbers} onClick={operatorHandler} className="w-16 h-16 rounded-3xl bg-violet-800 text-white text-xl font-bold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-violet-500 focus:outline focus:ring-2 focus:ring-violet-500  focus:ring-offset-2 focus:ring-offset-background">{numbers}</button>
+                    <button value={numbers} onClick={operatorHandler} className="
+                    w-16 h-16 rounded-3xl bg-violet-800 text-white text-xl font-bold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-violet-500 focus:outline focus:ring-2 focus:ring-violet-500  focus:ring-offset-2 focus:ring-offset-background
+                    
+                    ">
+                      {numbers}
+                    </button>
                   </div>
                 )
 
@@ -125,40 +139,67 @@ export function Home() {
                   if (numbers == 'C') {
                     return (
                       <div key={index}>
-                        <button value={numbers} onClick={clean} className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 focus:outline focus:ring-2 focus:ring-zinc-500  focus:ring-offset-2 focus:ring-offset-background">{numbers}</button>
+                        <button value={numbers} onClick={clean} className="
+                        w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 focus:outline focus:ring-2 focus:ring-zinc-500  focus:ring-offset-2 focus:ring-offset-background
+
+                        ">
+                          {numbers}
+                        </button>
                       </div>
                     )
                   }
                   else if (numbers == '%') {
                     return (
                       <div key={index}>
-                        <button value={numbers} onClick={porcetage} className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500  focus:ring-offset-2 focus:ring-offset-background">{numbers}</button>
+                        <button value={numbers} onClick={porcetage} className="
+                        w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500  focus:ring-offset-2 focus:ring-offset-background
+
+                        ">
+                          {numbers}
+                        </button>
                       </div>
                     )
                   }
                   else if (numbers == '+/-') {
                     return (
                       <div key={index}>
-                        <button value={numbers} onClick={changeSignal} className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 focus:ring-offset-background">{numbers}</button>
+                        <button value={numbers} onClick={changeSignal} className="
+                        w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 focus:ring-offset-background
+                        ">
+                          {numbers}
+                        </button>
                       </div>
                     )
                   }
                   return (
                     <div key={index}>
-                      <button value={numbers} className="w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 focus:ring-offset-background" >{numbers}</button>
+                      <button value={numbers} className="
+                      w-16 h-16 rounded-3xl bg-zinc-500 text-white text-xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-300 duration-300 focus:outline focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 focus:ring-offset-background
+                      ">
+                        {numbers}
+                      </button>
                     </div>
                   )
                 }
                 else if (numbers == '=') {
                   return (
                     <div key={index}>
-                      <button value={numbers} onClick={calculate} className="w-36 h-16 rounded-3xl bg-violet-800 text-white text-3xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-violet-500 focus:outline focus:ring-2 focus:ring-violet-800 focus:ring-offset-2 focus:ring-offset-background" >{numbers}</button>
+                      <button value={numbers} onClick={calculate} className="
+                      w-36 h-16 rounded-3xl bg-violet-800 text-white text-3xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-violet-500 focus:outline focus:ring-2 focus:ring-violet-800 focus:ring-offset-2 focus:ring-offset-background
+                      xl:w-36 xl:h-16
+                      ">
+                        {numbers}
+                      </button>
                     </div>
                   )
                 }
                 return (
                   <div key={index} >
-                    <button name={numbers} value={numbers} onClick={() => handleClickNumber(numbers)} className="w-16 h-16 rounded-3xl bg-zinc-800 text-white text-2xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-600 hover:border-zinc-300 focus:outline focus:ring-2 focus:ring-zinc-800 focus:ring-offset-2 focus:ring-offset-background">{numbers}</button>
+                    <button name={numbers} value={numbers} onClick={() => handleClickNumber(numbers)} className="
+                    w-16 h-16 rounded-3xl bg-zinc-800 text-white text-2xl font-semibold shadow-lg shadow-purple-900 dark:shadow-violet-700/50 transition hover:bg-zinc-600 hover:border-zinc-300 focus:outline focus:ring-2 focus:ring-zinc-800 focus:ring-offset-2 focus:ring-offset-background
+                    ">
+                      {numbers}
+                    </button>
                   </div>
                 )
               }
